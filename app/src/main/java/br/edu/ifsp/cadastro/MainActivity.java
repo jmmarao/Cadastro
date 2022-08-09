b9e3ae64acd8710ac9b0a851dd742f204479d480
jmmarao <jmmarao@gmail.com>
2022-08-08 22:06:58 -0300

    Spinner logic implemented

--- app/src/main/java/br/edu/ifsp/cadastro/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/cadastro/MainActivity.java
@@ -3,12 +3,18 @@ package br.edu.ifsp.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(new ArrayAdapter<UF>(this, android.R.layout.simple_spinner_item, UF.values()));
     }
 }
